3f71a36e6f1a09a6de1fad8175d4df9e673c1823
Parsa Emami <parsaemami65@gamil.com>
2025-05-08 01:41:37 +0330

    the navbar is designed and responsive is working --- alpha version

--- src/JS/functions/recommendedGetter.js
+++ src/JS/functions/recommendedGetter.js
@@ -2,17 +2,21 @@ let recommendedGetter = async () => {
   let req = await fetch("http://localhost:3001/games");
   let res = await req.json();
   let data = res.map((elem) => {
-    return `<div class="card w-[300px] h-[400px] bg-red-500 flex flex-col relative">
-<img src="${elem.image}" alt="${elem.title}" class="w-[300px] h-[170px]">
-<div class="square w-full h-full bg-amber-600 p-6 group duration-300">
-  <p class="gameName">${elem.gameName}</p>
-  <p class="title">${elem.title}</p>
-  <p class="description">${elem.desc}</p>
-  <div class="w-full h-fit bg-red-400">
-    <p class="price absolute bottom-6">${elem.price}</p>
-  </div>
-</div>
-</div>`;
+    return `<div class="card w-[300px] h-[400px] bg-[#292b33] flex flex-col">
+      <img src="${elem.image}" alt="pic" class="w-full h-[169.19px]">
+      <div class="w-full h-[230.42px] p-6 flex flex-col gap-2 relative">
+        <p class="text-[12px] text-[#ffffffB8] font-[object-sans]"> ${elem.gameName} </p>
+        <p class="title text-[16px] text-[#ffffffD6] font-[object-sans]">${elem.title}</p>
+        <p class="desc text-[12px] text-[#ffb400] font-[nato-sans-reg] line-clamp-2">${elem.desc}</p>
+        <p class="category text-[12px] text-[#ffffffB8] font-[nato-sans-reg]">${elem.category}</p>
+        <div class="price w-full h-fit mt-4 flex justify-between items-center absolute bottom-2 left-0 px-6">
+          <p class="text-[16px] text-[#6cdb00] font-[object-sans] inline-block">${elem.price}</p>
+          <div class="discount w-[45.38px] h-[20.5px] bg-[#6CDB00] rounded-[4px] flex justify-center
+          items-center">
+        <p class="text-[12px] text-[#1a1c21] font-[object-sans]">-40%</p></div>
+        </div>
+    </div>
+    </div>`;
   });
   document
     .querySelector("#recommendedGames")
--- src/JS/main.js
+++ src/JS/main.js
@@ -1,6 +1,8 @@
 import recommended from "./components/recommended";
 import recommendedGetter from "./functions/recommendedGetter";
 import Rootwriter from "./functions/rootwriter";
+import navbar from "./components/navbar.js";
 
-// Rootwriter(recommended);
-// recommendedGetter();
+
+Rootwriter(navbar,recommended);
+recommendedGetter();
