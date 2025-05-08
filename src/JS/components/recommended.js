f6b9a5b0044a5002b7f847ea3094bab8550e32dd
Parsa Emami <parsaemami65@gamil.com>
2025-05-08 21:31:06 +0330

    the left panel is not totally designed ! --- alpha version

--- src/JS/components/recommended.js
+++ src/JS/components/recommended.js
@@ -1,4 +1,4 @@
-let recommended = `<div id="recommendedGames" class="max-w-[1600px] mx-auto mt-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-[400px] gap-6 p-6 bg-gray-900">
+let recommended = `<div id="recommendedGames" class="max-w-[1600px] mx-auto mt-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-[400px] gap-6 p-6">
 </div>`;
 
 export default recommended;
--- src/JS/main.js
+++ src/JS/main.js
@@ -3,6 +3,16 @@ import recommendedGetter from "./functions/recommendedGetter";
 import Rootwriter from "./functions/rootwriter";
 import navbar from "./components/navbar.js";
 
+// Rootwriter(navbar,recommended);
+// recommendedGetter();
 
-Rootwriter(navbar,recommended);
-recommendedGetter();
+const hamburgerMenu = document.querySelector("#hamburgerMenubtn");
+const hamburgerMenuItems = document.querySelector("#hamburgerMenuItems");
+const closeBtnTimes = document.querySelector("#closeBtnTimes");
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenuItems.style.left = "0";
+});
+
+closeBtnTimes.addEventListener("click", () => {
+    hamburgerMenuItems.style.left = "-100%";
+});
