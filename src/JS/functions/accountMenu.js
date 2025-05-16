abcd0b65dcd289dbeb56f0b59830916fe8add85d
Parsa Emami <parsaemami65@gamil.com>
2025-05-17 02:46:53 +0330

    fixed: panles !

--- src/JS/functions/accountMenu.js
+++ src/JS/functions/accountMenu.js
@@ -1,14 +1,19 @@
 let accountBtn = () => {
-  let accountBtn = document.querySelector("#accountBtn");
-  let accountBtnPanel = document.querySelector("#accountBtnPanel");
-  let hamburgerMenuItems = document.querySelector("#hamburgerMenuItems");
+  const accountBtn = document.querySelector("#accountBtn");
+  const accountBtnPanel = document.querySelector("#accountBtnPanel");
+  const hamburgerMenuItems = document.querySelector("#hamburgerMenuItems");
+  const closebtnAccount = document.querySelector("#closebtnAccount");
   accountBtn.addEventListener("click", () => {
     if (hamburgerMenuItems.classList.contains("left-[0]")) {
-        hamburgerMenuItems.classList.remove("left-[0]");
-        hamburgerMenuItems.classList.add("left-[-100%]");
-    }  
+      hamburgerMenuItems.classList.remove("left-[0]");
+      hamburgerMenuItems.classList.add("left-[-100%]");
+    }
     accountBtnPanel.classList.add("right-[0]");
     console.log(hamburgerMenuItems);
+  });
+  closebtnAccount.addEventListener("click", () => {
+    accountBtnPanel.classList.remove("right-[0]");
+    accountBtnPanel.classList.add("right-[-100%]");
   });
 };
 
--- src/JS/functions/hamburgerMenu.js
+++ src/JS/functions/hamburgerMenu.js
@@ -2,9 +2,13 @@ let hamburgerMenu = () => {
   const hamburgerMenu = document.querySelector("#hamburgerMenubtn");
   const hamburgerMenuItems = document.querySelector("#hamburgerMenuItems");
   const closeBtnTimes = document.querySelector("#closeBtnTimes");
-
+  const accountBtnPanel = document.querySelector("#accountBtnPanel");
   hamburgerMenu.addEventListener("click", () => {
-    hamburgerMenuItems.classList.remove("left-[-100%]");
+    if (accountBtnPanel.classList.contains("right-[0]")) {
+      accountBtnPanel.classList.remove("right-[0]");
+      accountBtnPanel.classList.add("right-[-100%]");
+      hamburgerMenuItems.classList.add("left-[0]");
+    }
     hamburgerMenuItems.classList.add("left-[0]");
   });
 
