dbd9c8654bdd3d7c33a320313ec2605efbf96eee
Parsa Emami <parsaemami65@gamil.com>
2025-05-25 01:05:08 +0330

    merge branch

--- src/JS/functions/scroll.js
+++ src/JS/functions/scroll.js
@@ -11,22 +11,4 @@ let scroll = () => {
   });
 };
 
-// export default scroll;
-// let scroll = () => {
-//   window.addEventListener("scroll", () => {
-//     let container = document.querySelector(".search_container");
-
-//     // Check if screen width is at least 1024px (lg breakpoint)
-//     if (window.matchMedia("(min-width: 720px)").matches) {
-//       if (window.scrollY > 50) {
-//         container.classList.add("translate-y-[-75px]");
-//         container.classList.add("duration-200");
-//       } else {
-//           container.classList.add("duration-200");
-//         container.classList.remove("translate-y-[-75px]");
-//       }
-//     }
-//   });
-// };
-
 export default scroll;
