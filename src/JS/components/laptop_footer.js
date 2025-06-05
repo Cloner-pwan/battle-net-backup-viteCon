bbe6d1f52364dd87a68e58198a498177157e6c0b
Parsa Emami <parsaemami65@gamil.com>
2025-06-05 03:39:31 +0330

    add: footer || remains: make the footer responsive, make the slider responsive more accurate and precise, don't forget about the DOM manipulation

---
+++ src/JS/components/laptop_footer.js
@@ -0,0 +1,28 @@
+let laptop_footer = `<div class="laptopFooter_container w-full 2xl:h-[567px] xl:h-[567px] lg:h-[567px] md:h-[840px] bg-[url('../img/laptop-background.png')] bg-no-repeat bg-center mt-[60px] flex justify-center items-center py-[64px] px-[40px] bg-cover">
+      <div class="main_content_container w-[1400px] max-w-[1400px] h-[439px] grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 2xl:grid-row-1 xl:grid-rows-1 lg:grid-rows-1 md:grid-rows-[auto_auto] sm:grid-rows-2 2xl:gap-20
+      md:gap-2 sm:gap-10 place-items-center">
+        <div class="laptop_content 2xl:w-[792px] 2xl:h-[439px] xl:w-[692px] xl:h-[389px] lg:w-[592px] lg:h-[350px] flex justify-center items-center 2xl:order-none xl:order-none lg:order-none md:order-last sm:order-last">
+          <img src="./src/img/laptop.png" alt="laptop pic" class="w-full h-full">
+        </div>
+        <div class="text-format 2xl:w-[528px] xl:w-[428px] lg:w-[328px] md:w-[428px] sm:w-[390px] h-[300px] grid grid-rows-3">
+          <div class="heading_text w-full h-fit 2xl:place-self-end  md:place-self-center ">
+            <p class="2xl:text-[48px] 2xl:text-start xl:text-start lg:text-start xl:text-[32px] lg:text-[30px] md:text-[38px] md:text-center sm:text-[36px] sm:text-center text-[#fff] font-[object-sans]">Dive into adventure</p>
+            <p class="2xl:text-[20px] xl:text-[18px] xl:text-start lg:text-start lg:text-[16px] md:text-[16px] md:text-center sm:text-[15px] sm:text-center text-[#ffffffB8] font-[nato-sans-reg]">Download Battle.net and join a community of millions.</p>
+          </div>
+          <div class="btns_container w-full h-fit 2xl:place-self-end xl:place-self-end lg:place-self-end md:place-self-center sm:place-self-center flex 2xl:flex-wrap xl:flex-wrap lg:flex-wrap md:flex-nowrap sm:flex-wrap 2xl:justify-start xl:justify-start lg:justify-start sm:justify-center gap-2">
+              <a href="#" class="w-[283.5px] h-10 bg-[#0074e0] text-[16px] text-[object-sans] font-bold font-[#fff] py-1.5 px-8 col-span-1 rounded-sm flex justify-center items-center gap-2 duration-300
+              border-2 border-transparent hover:border-[#2290ff]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" class="w-5 h-5" viewBox="0 0 32 32" version="1.1">
+<title>microsoft</title>
+<path d="M16.742 16.742v14.253h14.253v-14.253zM1.004 16.742v14.253h14.256v-14.253zM16.742 1.004v14.256h14.253v-14.256zM1.004 1.004v14.256h14.256v-14.256z"/>
+                </svg>
+                Download for Windows</a>
+            <a href="#" class="2xl:w-[155.5px] xl:w-[155.5px] lg:w-[155.5px] md:w-[155.5px] sm:w-[283.5px] h-[40px] bg-[#2A2D31] place-self-center flex justify-center items-center rounded-sm text-[16px] text-white font-[object-sans] duration-300 border-1 border-transparent hover:border-[#878787]">Learn More</a>
+          </div>
+          <div class="available w-full h-fit 2xl:place-self-end md:place-self-center pt-5">
+            <p class="2xl:text-[16px] 2xl:text-start xl:text-start lg:text-start md:text-[18px] md:text-center sm:text-center text-[#ffffffB8] font-[nato-sans-reg]">Also available for <a href="#" class="text-[#148eff] underline underline-offset-4">Mac </a> and <a href="#" class="text-[#148eff] underline underline-offset-4">mobile</a></p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+export default laptop_footer;
