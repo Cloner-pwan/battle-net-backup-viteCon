90969eff478ae2c7d4d60ac52ac02cad8ff0ab28
Parsa Emami <parsaemami65@gamil.com>
2025-06-06 00:34:08 +0330

    add: footer => only the first part is responsive || remains: responsive the second part

---
+++ src/JS/components/fixedbtn.js
@@ -0,0 +1,5 @@
+let fixedbtn = `<a href="#" class="fixedbtn w-11 h-11 bg-[#15171E] outline outline-[#535353] border border-transparent hover:border-[#707070] rounded-sm fixed z-[99999] bottom-10 right-10 flex justify-center items-center duration-300 cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-12" fill="currentColor" viewBox="0 0 24 24" part="icon"><path d="M18.646 14.646a.5.5 0 0 1 0 .707l-.703.704a.5.5 0 0 1-.707 0L12 10.83l-5.236 5.227a.5.5 0 0 1-.707 0l-.703-.703a.5.5 0 0 1 0-.708l6.292-6.293a.5.5 0 0 1 .708 0l6.293 6.293Z"></path></svg>
+    </a>`;
+    
+export default fixedbtn;
